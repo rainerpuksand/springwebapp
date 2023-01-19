0eb50cd3520731bcddbb26af7cfca067a443147b
Rainer <rainerpuksand@hotmail.com>
2023-01-19 15:06:05 +0200

    two POJO from Guru,repository's,Data

--- src/main/java/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,4 +1,45 @@
 package springframework.spring5webapp.bootstrap;
 
-public class BootStrapData implements {
+import org.springframework.boot.CommandLineRunner;
+import springframework.spring5webapp.domain.Author;
+import springframework.spring5webapp.domain.Book;
+import springframework.spring5webapp.repositories.AuthorRepository;
+import springframework.spring5webapp.repositories.BookRepository;
+
+public class BootStrapData implements CommandLineRunner {
+
+
+    private  final AuthorRepository authorRepository;
+    private  final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Desing", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "56847547");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+
+    }
 }
--- src/main/java/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -3,5 +3,5 @@ package springframework.spring5webapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import springframework.spring5webapp.domain.Author;
 
-public interface AuthorRepositories extends CrudRepository <Author, Long>{
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
